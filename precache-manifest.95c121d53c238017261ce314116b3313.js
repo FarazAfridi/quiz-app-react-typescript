11c5c9963e122b2a58c176867e3c072183eb073c
FarazAfridi <farazafridi5159@gmail.com>
2020-09-26 15:54:59 +0500

    Updates

---
+++ precache-manifest.95c121d53c238017261ce314116b3313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059da86b5c59e8a667ec771039cc9d28",
+    "url": "/FarazAfridi/quiz-app-react-typescript/index.html"
+  },
+  {
+    "revision": "2c188e62e999f2c102f0",
+    "url": "/FarazAfridi/quiz-app-react-typescript/static/css/main.4b1f0e93.chunk.css"
+  },
+  {
+    "revision": "db4fe614c2880f0adc0a",
+    "url": "/FarazAfridi/quiz-app-react-typescript/static/js/2.dfcdf73a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FarazAfridi/quiz-app-react-typescript/static/js/2.dfcdf73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c188e62e999f2c102f0",
+    "url": "/FarazAfridi/quiz-app-react-typescript/static/js/main.eb0f08e6.chunk.js"
+  },
+  {
+    "revision": "90465936e19cf40c8096",
+    "url": "/FarazAfridi/quiz-app-react-typescript/static/js/runtime-main.b4122121.js"
+  }
+]);
